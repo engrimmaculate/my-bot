e02f594c116201f968ea2f312ae8dd58ebce0c0f
engrimmaculate <hiimmaculate.ci@wgmail.com>
2023-07-14 11:18:28 +0100

    completed the Coin CoinItem Diplay Static Data

--- App.js
+++ App.js
@@ -1,11 +1,179 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View,Image } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.coinContainer}>
+         {/* <Text style={{ color:'white' }}>Hello World!</Text> */}
+          <Image source={{ uri: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579' }} 
+          style={styles.coinImage} />
+          
+          <View>
+            <Text style={ styles.title }>  BitCoin </Text>
+                <View style={{flexDirection:'row'}}>
+                <View style={styles.rankContainer}>
+                  <Text style={ styles.rank }> 1 </Text>
+                </View>
+                <Text style={ styles.text }>  BTC </Text>
+                    <AntDesign name="caretup" size={12} color="white" style={{ alignSelf: 'center' }} />
+                    <Text style={ styles.text }>  0.63% </Text>
+                </View>
+          </View>
+          <View style ={{ marginLeft: 'auto' }}>
+              <Text style={styles.title}> 56,536.09</Text>
+              <Text style={styles.text}>MCap 1.076 T</Text>
+          </View>
+      </View>
+      <View style={styles.coinContainer}>
+         {/* <Text style={{ color:'white' }}>Hello World!</Text> */}
+          <Image source={{ uri: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579' }} 
+          style={styles.coinImage} />
+          
+          <View>
+            <Text style={ styles.title }>  BitCoin </Text>
+                <View style={{flexDirection:'row'}}>
+                <View style={styles.rankContainer}>
+                  <Text style={ styles.rank }> 1 </Text>
+                </View>
+                <Text style={ styles.text }>  BTC </Text>
+                    <AntDesign name="caretup" size={12} color="white" style={{ alignSelf: 'center' }} />
+                    <Text style={ styles.text }>  0.63% </Text>
+                </View>
+          </View>
+          <View style ={{ marginLeft: 'auto' }}>
+              <Text style={styles.title}> 56,536.09</Text>
+              <Text style={styles.text}>MCap 1.076 T</Text>
+          </View>
+      </View>
+      <View style={styles.coinContainer}>
+         {/* <Text style={{ color:'white' }}>Hello World!</Text> */}
+          <Image source={{ uri: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579' }} 
+          style={styles.coinImage} />
+          
+          <View>
+            <Text style={ styles.title }>  BitCoin </Text>
+                <View style={{flexDirection:'row'}}>
+                <View style={styles.rankContainer}>
+                  <Text style={ styles.rank }> 1 </Text>
+                </View>
+                <Text style={ styles.text }>  BTC </Text>
+                    <AntDesign name="caretup" size={12} color="white" style={{ alignSelf: 'center' }} />
+                    <Text style={ styles.text }>  0.63% </Text>
+                </View>
+          </View>
+          <View style ={{ marginLeft: 'auto' }}>
+              <Text style={styles.title}> 56,536.09</Text>
+              <Text style={styles.text}>MCap 1.076 T</Text>
+          </View>
+      </View>
+      <View style={styles.coinContainer}>
+         {/* <Text style={{ color:'white' }}>Hello World!</Text> */}
+          <Image source={{ uri: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579' }} 
+          style={styles.coinImage} />
+          
+          <View>
+            <Text style={ styles.title }>  BitCoin </Text>
+                <View style={{flexDirection:'row'}}>
+                <View style={styles.rankContainer}>
+                  <Text style={ styles.rank }> 1 </Text>
+                </View>
+                <Text style={ styles.text }>  BTC </Text>
+                    <AntDesign name="caretup" size={12} color="white" style={{ alignSelf: 'center' }} />
+                    <Text style={ styles.text }>  0.63% </Text>
+                </View>
+          </View>
+          <View style ={{ marginLeft: 'auto' }}>
+              <Text style={styles.title}> 56,536.09</Text>
+              <Text style={styles.text}>MCap 1.076 T</Text>
+          </View>
+      </View>
+      <View style={styles.coinContainer}>
+         {/* <Text style={{ color:'white' }}>Hello World!</Text> */}
+          <Image source={{ uri: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579' }} 
+          style={styles.coinImage} />
+          
+          <View>
+            <Text style={ styles.title }>  BitCoin </Text>
+                <View style={{flexDirection:'row'}}>
+                <View style={styles.rankContainer}>
+                  <Text style={ styles.rank }> 1 </Text>
+                </View>
+                <Text style={ styles.text }>  BTC </Text>
+                    <AntDesign name="caretup" size={12} color="white" style={{ alignSelf: 'center' }} />
+                    <Text style={ styles.text }>  0.63% </Text>
+                </View>
+          </View>
+          <View style ={{ marginLeft: 'auto' }}>
+              <Text style={styles.title}> 56,536.09</Text>
+              <Text style={styles.text}>MCap 1.076 T</Text>
+          </View>
+      </View>
+      <View style={styles.coinContainer}>
+         {/* <Text style={{ color:'white' }}>Hello World!</Text> */}
+          <Image source={{ uri: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579' }} 
+          style={styles.coinImage} />
+          
+          <View>
+            <Text style={ styles.title }>  BitCoin </Text>
+                <View style={{flexDirection:'row'}}>
+                <View style={styles.rankContainer}>
+                  <Text style={ styles.rank }> 1 </Text>
+                </View>
+                <Text style={ styles.text }>  BTC </Text>
+                    <AntDesign name="caretup" size={12} color="white" style={{ alignSelf: 'center' }} />
+                    <Text style={ styles.text }>  0.63% </Text>
+                </View>
+          </View>
+          <View style ={{ marginLeft: 'auto' }}>
+              <Text style={styles.title}> 56,536.09</Text>
+              <Text style={styles.text}>MCap 1.076 T</Text>
+          </View>
+      </View>
+      <View style={styles.coinContainer}>
+         {/* <Text style={{ color:'white' }}>Hello World!</Text> */}
+          <Image source={{ uri: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579' }} 
+          style={styles.coinImage} />
+          
+          <View>
+            <Text style={ styles.title }>  BitCoin </Text>
+                <View style={{flexDirection:'row'}}>
+                <View style={styles.rankContainer}>
+                  <Text style={ styles.rank }> 1 </Text>
+                </View>
+                <Text style={ styles.text }>  BTC </Text>
+                    <AntDesign name="caretup" size={12} color="white" style={{ alignSelf: 'center' }} />
+                    <Text style={ styles.text }>  0.63% </Text>
+                </View>
+          </View>
+          <View style ={{ marginLeft: 'auto' }}>
+              <Text style={styles.title}> 56,536.09</Text>
+              <Text style={styles.text}>MCap 1.076 T</Text>
+          </View>
+      </View>
+      <View style={styles.coinContainer}>
+         {/* <Text style={{ color:'white' }}>Hello World!</Text> */}
+          <Image source={{ uri: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579' }} 
+          style={styles.coinImage} />
+          
+          <View>
+            <Text style={ styles.title }>  BitCoin </Text>
+                <View style={{flexDirection:'row'}}>
+                <View style={styles.rankContainer}>
+                  <Text style={ styles.rank }> 1 </Text>
+                </View>
+                <Text style={ styles.text }>  BTC </Text>
+                    <AntDesign name="caretup" size={12} color="white" style={{ alignSelf: 'center' }} />
+                    <Text style={ styles.text }>  0.63% </Text>
+                </View>
+          </View>
+          <View style ={{ marginLeft: 'auto' }}>
+              <Text style={styles.title}> 56,536.09</Text>
+              <Text style={styles.text}>MCap 1.076 T</Text>
+          </View>
+      </View>
+
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +181,44 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#121212',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    paddingTop:50,
+  },
+  title :{
+    color: 'white',
+    fontSize:18,
+    fontWeight: 'bold',
+    marginBottom: 3
+  },
+  text:{
+    color: 'white',
+    marginRight: 10,
+    alignSelf:'center',
+  },
+  coinContainer: {
+    flexDirection: 'row',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: 'grey', //'#282828', 
+    padding:15,
+  },
+  coinImage : {
+     height:30,
+     width:30, 
+     marginRight:10, 
+     alignSelf:'center',
+     marginLeft: 20, 
+    },
+  rank: {
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  rankContainer : {
+    backgroundColor:'#585858',
+    paddingHorizontal:5,
+    borderRadius:2,
+    marginRight: 5,
+    marginLeft:10,
   },
 });
